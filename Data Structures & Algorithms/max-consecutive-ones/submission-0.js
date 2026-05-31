3eab3ccd324334ddecd2cc9e3ad46b75cca1a519
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-05-31 14:32:35 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let max = 0;
+        let count = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count > max) {
+                max = count;
+            }
+        }
+        return max;
+    }
+}
