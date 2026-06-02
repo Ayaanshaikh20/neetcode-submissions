0d40426a44d2d787c5f1cb59ae7ac853700f62c6
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-02 13:01:48 +0530

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+        let hash = {};
+        for(let i=0; i<s.length; i++) {
+            if(!hash[s[i]] && !(hash[s[i]] in hash)) {
+                hash[s[i]] = t[i];
+            } else{
+                if(hash[s[i]] == t[i]) {
+                    continue
+                } else {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
