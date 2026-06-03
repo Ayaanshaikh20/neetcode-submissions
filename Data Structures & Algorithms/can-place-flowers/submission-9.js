6cd64c0d5c967d8a5738bfc21f8b13d9ab51301f
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-03 11:15:32 +0530

    Add: can-place-flowers - submission-9

---
+++ Data Structures & Algorithms/can-place-flowers/submission-9.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} flowerbed
+     * @param {number} n
+     * @return {boolean}
+     */
+    canPlaceFlowers(flowerbed, n) {
+        let left = 0;
+        let right = 0;
+        for (let i = 0; i < flowerbed.length; i++) {
+            left = i== 0 ? 0 : flowerbed[i - 1];
+            right = i == flowerbed.length-1 ? 0 : flowerbed[i + 1];
+            if (flowerbed[i] == 0 && left == 0 && right == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            if (n == 0) {
+                console.log('inside')
+                return true;
+            }
+        }
+        if (n == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
