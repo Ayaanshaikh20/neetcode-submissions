18f8f6a34f95acf163e9fda61b75bdbff9992895
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-05 23:53:15 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-7

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-7.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    maxDifference(s) {
+        let hash = {};
+        let even = Infinity;
+        let odd = 0;
+        for(let i=0; i<s.length; i++) {
+            if(!hash[s[i]]) {
+                hash[s[i]] = 1;
+            } else {
+                hash[s[i]] += 1;
+            }
+        }
+        console.log(hash)
+        for(let al in hash) {
+            if(hash[al] %2 ==0 && hash[al] < even) {
+                even = hash[al]
+            } else if (hash[al] %2 !== 0 && hash[al] > odd) {
+                odd = hash[al]
+            }
+        }
+        
+        return odd-even
+    }
+}
