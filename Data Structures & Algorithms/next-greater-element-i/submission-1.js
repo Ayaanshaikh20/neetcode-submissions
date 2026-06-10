d17d6f212ec05274ea8e3a77054ce8eac27cb212
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-10 10:47:44 +0530

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    nextGreaterElement(nums1, nums2) {
+        let hash = {};
+        let res = [];
+        for (let i = 0; i < nums1.length; i++) {
+            hash[nums1[i]] = i;
+        }
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums2[j] in hash) {
+                for (let k = 1; k < nums2.length; k++) {
+                    if (nums2[k] > nums2[j]) {
+                        res[hash[nums2[j]]] = nums2[k];
+                        break
+                    } else {
+                        res[hash[nums2[j]]] = -1;
+                        break
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
