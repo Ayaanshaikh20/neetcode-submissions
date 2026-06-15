a84126f7987b4b8ce77a8ef51af1f6f08dfe9e63
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-15 14:20:06 +0530

    Add: next-greater-element-i - submission-11

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-11.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    nextGreaterElement(nums1, nums2) {
+        let res = Array.from({ length: nums1.length });
+        let stack = [];
+        let hash = {};
+        for (let j = 0; j < nums1.length; j++) {
+            hash[nums1[j]] = j;
+        }
+        for (let i = nums2.length - 1; i >= 0; i--) {
+            let stackTop = stack[stack.length - 1];
+            while (nums2[i] > stackTop && stack.length > 0) {
+                stack.pop();
+                stackTop = stack[stack.length - 1];
+            }
+            if (nums2[i] in hash) {
+                if (stack.length == 0) {
+                    res[hash[nums2[i]]] = -1;
+                } else {
+                    res[hash[nums2[i]]] = stackTop;
+                }
+            }
+            stack.push(nums2[i]);
+        }
+        return res;
+    }
+}
