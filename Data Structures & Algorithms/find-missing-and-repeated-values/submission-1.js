3b5925dec2bed8134c18c3dc196f44f3a3a484ee
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-21 21:14:05 +0530

    Add: find-missing-and-repeated-values - submission-1

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number[]}
+     */
+    findMissingAndRepeatedValues(grid) {
+        let res = grid.flat();
+        let hash = {};
+        let final = [];
+        for (let i = 0; i < res.length; i++) {
+            if (!hash[res[i]]) {
+                hash[res[i]] = 1;
+            } else {
+                hash[res[i]] += 1;
+            }
+        }
+        for (let i=1; i<=res.length; i++) {
+            if(!(i in hash)) {
+                hash[i] = 0;
+            }
+        }
+        for(let i in hash) {
+            if(hash[i] > 1) {
+                final[0] = i
+            } else if (hash[i] == 0) {
+                final[1] = i
+            }
+        }
+        return final;
+    }
+}
