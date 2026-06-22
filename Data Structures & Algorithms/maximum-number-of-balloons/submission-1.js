8e4d581b42b54197693535e98d8f501736ccf970
AyaanShaikh <90324206+Ayaanshaikh20@users.noreply.github.com>
2026-06-22 15:48:19 +0530

    Add: maximum-number-of-balloons - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} text
+     * @return {number}
+     */
+    maxNumberOfBalloons(text) {
+        let required = {
+            b: 1,
+            a: 1,
+            l: 2,
+            o: 2,
+            n: 1,
+        };
+        let hash = {};
+        let count = Infinity;
+        for (let i = 0; i < text.length; i++) {
+            if (!hash[text[i]]) {
+                hash[text[i]] = 1;
+            } else {
+                hash[text[i]] += 1;
+            }
+        }
+        for(let i in required) {
+            let available = Math.floor(hash[i]/required[i]);
+            if(available < count) {
+                count = available;
+            }
+        }
+        if(count == Infinity) {
+            return 0
+        } else {
+            return count;
+        }
+    }
+}
